26ea5781d36ec88e17a30265ec4acb5e56118da6
Taz Campbell <shelbyacampbell2724@gmail.com>
2024-03-31 13:45:41 -0400

    second migration made

--- appointments/list/models.py
+++ appointments/list/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Appointments(models.Model):
+    title=models.CharField(max_length=50)
+    date=models.CharField(max_length=20)
+    time=models.CharField(max_length=10)
